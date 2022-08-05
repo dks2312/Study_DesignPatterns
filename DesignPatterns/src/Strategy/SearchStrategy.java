a334e7bcb6d0ff63cacd8da6f93b5ce8f68e703a
DKS2312 <dtg2312@gmail.com>
2022-08-05 16:47:05 +0900

    스트레티지 패턴 코드 추가

---
+++ DesignPatterns/src/Strategy/SearchStrategy.java
@@ -0,0 +1,37 @@
+package Strategy;
+
+interface SearchStrategy {
+	public void search();
+}
+
+class SearchStrategyAll implements SearchStrategy{
+	@Override
+	public void search() {
+		System.out.println("SEARCH ALL");
+		/* 전체 검색하는 코드 */
+	}
+}
+
+class SearchStrategyImage implements SearchStrategy{
+	@Override
+	public void search() {
+		System.out.println("SEARCH IMAGE");
+		/* 이미지 검색하는 코드 */
+	}
+}
+
+class SearchStrategyNews implements SearchStrategy{
+	@Override
+	public void search() {
+		System.out.println("SEARCH NEWS");
+		/* 뉴스 검색하는 코드 */
+	}
+}
+
+class SearchStrategyMap implements SearchStrategy{
+	@Override
+	public void search() {
+		System.out.println("SEARCH MAP");
+		/* 지도 검색하는 코드 */
+	}
+}
