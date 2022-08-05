bba5f863f99ccc4b9e0cd2fb40635340e0ce8f71
DKS2312 <dtg2312@gmail.com>
2022-08-05 18:58:42 +0900

    스태이터 패턴 코드 추가

---
+++ DesignPatterns/src/State/MyProgram.java
@@ -0,0 +1,15 @@
+package State;
+
+public class MyProgram {
+
+	public static void main(String[] args) {
+		final ModeSwitch modeSwitch = new ModeSwitch();
+
+		modeSwitch.onSwitch();
+		modeSwitch.onSwitch();
+		modeSwitch.onSwitch();
+		modeSwitch.onSwitch();
+		modeSwitch.onSwitch();
+	}
+
+}
